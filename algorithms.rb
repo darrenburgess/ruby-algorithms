8e60eae7edee085f76f0c8caa42518ffcce08b7b
Darren Burgess <darren_b@beezwax.net>
2015-11-13 09:18:18 -0500

    initial commit: refactored ruby koans

---
+++ algorithms.rb
@@ -0,0 +1,14 @@
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+
+class BasicAlgorithms < Neo::Koan
+
+  def reverse_a_string (string)
+
+  end
+
+  def test_reverse_a_string
+    string = "hello world!"
+    result = reverse_a_string(string)
+    assert_equal result, "!dlrow olleh"
+  end
+end
---
+++ path_to_enlightenment.rb
@@ -0,0 +1,6 @@
+# The path to Ruby Enlightenment starts with the following:
+
+$LOAD_PATH << File.dirname(__FILE__)
+
+require 'algorithms'
+
